284251e68bc74ad8132d1de43fb04675524f0406
Robert Andres Ramos Astudillo <rramos.playtechla@gmail.com>
2020-10-07 16:02:00 -0500

    Se trabajo en la parte del routing y maquetacion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,10 +1,48 @@
 import React from "react"
-import Header from "./header"
 import Helmet from "react-helmet"
+import { Global, css } from '@emotion/core';
+import Header from "./header"
+
 
 const Layout = ({ children }) => {
   return (
     <>
+      <Global
+        styles={css`
+          html {
+            font-size: 62.5%;
+          }
+
+          body {
+            font-size: 18px;
+            font-size: 1.8rem;
+            line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
+          }
+
+          h1,
+          h2,
+          h3 {
+            margin: 0;
+            line-height: 1.5;
+          }
+
+          h1,
+          h2 {
+            font-family: "Roboto", serif;
+          }
+
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
+
+          ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+          }
+        `}
+      />
       <Helmet>
         <title>Gatsby Hotel</title>
         <link
